cb3fb947a9f1711881db6aa50a1feb159abfc79f
Waleed Ahmad <137788903+Waleed-Ahmad-dev@users.noreply.github.com>
2025-12-01 18:53:54 +0500

    Create route.ts

---
+++ src/app/api/auth/google/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+     const GOOGLE_AUTH_URL = "https://accounts.google.com/o/oauth2/v2/auth";
+
+     const params = new URLSearchParams({
+          client_id: process.env.GOOGLE_CLIENT_ID!,
+          redirect_uri: process.env.GOOGLE_REDIRECT_URI!,
+          response_type: "code",
+          scope: "openid email profile",
+          access_type: "offline",
+          prompt: "consent",
+     });
+
+     return NextResponse.redirect(`${GOOGLE_AUTH_URL}?${params.toString()}`);
+}
