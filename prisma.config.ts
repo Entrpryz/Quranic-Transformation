b02d6a993be800de9b7d55e552a8b10afb63f38a
Waleed Ahmad <137788903+Waleed-Ahmad-dev@users.noreply.github.com>
2025-12-08 18:11:56 +0500

    Update prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,11 +1,10 @@
-// prisma.config.ts
 import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
-    seed: "ts-node prisma/seed.ts", // <--- Add this line
+    seed: "npx ts-node prisma/seed.ts", // <--- ADD THIS LINE
   },
   engine: "classic",
   datasource: {
