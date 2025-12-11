8ee69bc2f6ca2de59d96de9f86ef9b1bcbe35d14
Waleed Ahmad <137788903+Waleed-Ahmad-dev@users.noreply.github.com>
2025-12-11 18:03:01 +0500

    fixed some errors

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -15,6 +15,7 @@ export async function encrypt(payload: any) {
 }
 
 export async function decrypt(input: string): Promise<any> {
+  if (!input) return null;
   try {
     const { payload } = await jwtVerify(input, key, {
       algorithms: ["HS256"],
